d5675099e45f6211191d2d989b7c3be7d7e93b5c
g00s3_m4n <petrovih384@gmail.com>
2022-06-19 00:48:28 +0700

    first commit

---
+++ slider.js
@@ -0,0 +1,26 @@
+(function() {
+    'use strict';
+
+    const slider = document.querySelector('.slider');
+    const btnPrev = document.querySelector('.prev');
+    const btnNext = document.querySelector('.next');
+
+    let slideWidth = document.querySelector('.slide').offsetWidth;
+    let sliderWidth = slider.childElementCount * slideWidth;
+    let slideOffset = 0;
+
+    btnNext.onclick = function() {
+        slideOffset += slideWidth;
+
+        if(slideOffset >= sliderWidth) slideOffset = 0;
+
+        slider.style.left = -slideOffset + 'px';
+    };
+
+    btnPrev.onclick = function() {
+        if(slideOffset == 0) slideOffset = sliderWidth;
+        
+        slideOffset -= slideWidth;
+        slider.style.left = -slideOffset + 'px';
+    };
+})();
