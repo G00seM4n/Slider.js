b2118bee776761e8bd694155c9d904b3bc67e0f5
Никита <66599398+G00seM4n@users.noreply.github.com>
2022-06-19 02:24:43 +0700

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,26 +1,22 @@
-(function(){
-    'use strict';
+const slider = document.querySelector('.slider');
+const btnPrev = document.querySelector('.prev');
+const btnNext = document.querySelector('.next');
 
-    const slider = document.querySelector('.slider');
-    const btnPrev = document.querySelector('.prev');
-    const btnNext = document.querySelector('.next');
+let slideWidth = document.querySelector('.slide').offsetWidth;
+let sliderWidth = slider.childElementCount * slideWidth;
+let slideOffset = 0;
 
-    let slideWidth = document.querySelector('.slide').offsetWidth;
-    let sliderWidth = slider.childElementCount * slideWidth;
-    let slideOffset = 0;
+btnNext.onclick = function() {
+    slideOffset += slideWidth;
 
-    btnNext.onclick = function() {
-        slideOffset += slideWidth;
+    if(slideOffset >= sliderWidth) slideOffset = 0;
 
-        if(slideOffset >= sliderWidth) slideOffset = 0;
+    slider.style.left = -slideOffset + 'px';
+};
 
-        slider.style.left = -slideOffset + 'px';
-    };
+btnPrev.onclick = function() {
+    if(slideOffset == 0) slideOffset = sliderWidth;
 
-    btnPrev.onclick = function() {
-        if(slideOffset == 0) slideOffset = sliderWidth;
-
-        slideOffset -= slideWidth;
-        slider.style.left = -slideOffset + 'px';
-    };
-});
+    slideOffset -= slideWidth;
+    slider.style.left = -slideOffset + 'px';
+};
